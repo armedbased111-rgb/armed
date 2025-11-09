073ada2a5ba576abf3e3a15a972119b42d48cb43
armedzoran111111 <armedbased111@gmail.com>
2025-11-09 18:50:35 +0100

    CartContext Zustand

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,3 +1,4 @@
+// apps/web/src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -7,7 +8,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
   </React.StrictMode>
 );
